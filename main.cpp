5c8a35ce8543845382edec388a1ebd8cbe01f91d
y.dechaux <y.dechaux@student.isartdigital.com>
2022-02-02 11:25:55 +0100

    Vec4 added

--- main.cpp
+++ main.cpp
@@ -3,9 +3,19 @@
 
 int main()
 {
-    Vec3 test = {3.f,2.f,4.f};
+    Vec3 test = {3.f, 2.f, 4.f};
     float result = test.GetMagnitude();
+    printf("Vec3 Magnitude = %f\n", result);
+    Vec3 test2 = test * 2;
+    printf("Vec3 * opp = %f\n", test2.z);
+    Vec3 test3 = test + test2;
+    printf("Vec3 + opp = %f\n", test3.z);
+    test3.Normalize();
+      printf("Vec3 Normalise = %f\n", test3.z);
 
-    printf("%f\n",result);
+
+    
+    
+
     return 0;
 }
--- mathlib.cpp
+++ mathlib.cpp
@@ -1,9 +1,9 @@
 #include "mathlib.hpp"
 #include "math.h"
 
-float Vec3::GetMagnitude();
+float Vec3::GetMagnitude()
 {
-    return sqrt(pow(x,2)+pow(y,2)+pow(z,2));
+    return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
 }
 
 void Vec3::Normalize()
@@ -13,14 +13,61 @@ void Vec3::Normalize()
     z = z / GetMagnitude();
 }
 
-Vec3 Vec3::operator+ (const Vec3& rhs)
+Vec3 Vec3::operator+(const Vec3 &rhs)
 {
-    return {this->x+rhs.x,this->y+rhs.y,this->z+rhs.z};
+    return {this->x + rhs.x, this->y + rhs.y, this->z + rhs.z};
 }
 
-Vec3 Vec3::operator* (const float& rhs)
+Vec3 Vec3::operator*(const float &rhs)
 {
-    return {this->x*rhs,this->y*rhs,this->z*rhs};
+    return {this->x * rhs, this->y * rhs, this->z * rhs};
 }
 
+Vec4::Vec4(const Vec3 &vec3, float _w )
+{
+    x = vec3.x;
+    y = vec3.y;
+    z = vec3.z;
+    w = _w;
+}
 
+void Vec4::Homogenize()
+{
+    if (w != 0)
+    {
+        x = x / w;
+        y = y / w;
+        z = z / w;
+    }
+}
+
+float Vec4::GetMagnitude()
+{
+    Vec4 temp({x, y, z}, w);
+    temp.Homogenize();
+    return sqrt(pow(temp.x, 2) + pow(temp.y, 2) + pow(temp.z, 2));
+}
+
+void Vec4::Normalize()
+{
+    x = x / GetMagnitude();
+    y = y / GetMagnitude();
+    z = z / GetMagnitude();
+}
+
+Vec4 Vec4::operator+(const Vec4 &rhs)
+{
+    Vec4 temp({this->x + rhs.x, this->y + rhs.y, this->z + rhs.z}, this->w + rhs.w);
+    return temp;
+}
+
+Vec4 Vec4::operator*(const float &rhs)
+{
+    Vec4 temp({this->x * rhs, this->y * rhs, this->z * rhs}, this->w * rhs);
+    return temp;
+}
+
+Vec4::~Vec4()
+{
+    
+}
